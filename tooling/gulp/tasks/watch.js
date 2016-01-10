64f5b9eb544c080c1c89db05959d650a4dd6e14b
Joshua Fisher <Joshua Fisher>
2016-01-10 00:40:20 -0500

    Gulp task watch:dev had reference to transpile_ts:dev which is no longer a valid task. Changing to transiple_ts in order for live reload to no longer crash.

--- tooling/gulp/tasks/watch.js
+++ tooling/gulp/tasks/watch.js
@@ -12,7 +12,7 @@ gulp.task('watch:realtime', function(){
 
 // Watch for changes.
 gulp.task('watch:dev', function(){
-    gulp.watch("app/**/*.ts", ['lint_ts', 'transpile_ts:dev']).on('change', logChanges);
+    gulp.watch("app/**/*.ts", ['lint_ts', 'transpile_ts']).on('change', logChanges);
     gulp.watch("app/**/*.less", ['lint_less', 'transpile_less']).on('change', logChanges);
     gulp.watch("app/**/*.jade", ['lint_jade', 'transpile_jade:dev']).on('change', logChanges);
     gulp.watch(global.paths.dev_dist + '/**/*.*', ['serve:reload']).on('change', logChanges);
